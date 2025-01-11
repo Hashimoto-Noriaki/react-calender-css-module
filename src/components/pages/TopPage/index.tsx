2e5a7f0b56293805c69fd1596223d51ec0ff64c3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-12 01:08:26 +0900

    簡単な表示

---
+++ src/components/pages/TopPage/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export const TopPage = () => {
+  return (
+    <div className={`${styles.container}`}>
+        <h1 className={`${styles.underline}`}>犬</h1>
+    </div>
+  )
+}
