a4204dbae12cc4498ce0b293fa12b93ecbfd3614
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-18 20:00:14 +0900

    トップペーシ

--- src/components/pages/TopPage/index.tsx
+++ src/components/pages/TopPage/index.tsx
@@ -2,22 +2,18 @@ import styles from './styles.module.css';
 
 export const TopPage = () => {
   return (
-    <div>
-      <header>
-        <div className={`${styles.flex} ${styles.justifyBetween}`}>
-          <p>スケジュール管理App</p>
-          <nav>
-            <ul className={`${styles.flex} ${styles.gap5}`}>
-              <li>利用方法</li>
-              <li>ログイン</li>
+    <div className={styles.relative}>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <p className={styles.logo}>スケジュール管理APP</p>
+          <nav className={styles.nav}>
+            <ul className={styles.nav}>
+              <li className={styles.navItem}>ご利用方法</li>
+              <li className={styles.navItem}>ログイン</li>
             </ul>
           </nav>
         </div>
       </header>
     </div>
-  )
-}
-
-{/* <div className={`${styles.container}`}>
-        <h1 className={`${styles.underline}`}>犬</h1>
-    </div> */}
+  );
+};
